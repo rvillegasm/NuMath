8ac90d84b909c921533dc49ba1512551e49e85e1
Isa Muriel <isamuriel@MacBook-Air-de-Isa.local>
2019-09-17 14:00:50 -0500

    Fixed point implemented

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -1,59 +1,79 @@
 #include <math.h>
 #include <stdio.h>
 
-#include "singleVariableEquations/incrementalSearch.h"
-#include "singleVariableEquations/closedMethods/bisection.h"
-#include "singleVariableEquations/closedMethods/falsePosition.h"
+// #include "singleVariableEquations/incrementalSearch.h"
+// #include "singleVariableEquations/closedMethods/bisection.h"
+// #include "singleVariableEquations/closedMethods/falsePosition.h"
+#include "singleVariableEquations/openMethods/fixedPoint.h"
 
-#include "../lib/statusConstants.h"
+// #include "../lib/statusConstants.h"
 #include "../lib/exceptions.h"
 
 #define TOLERANCE 10e-10
 
 double f(double x);
+double g(double x);
 
 int main() {
 
-    Interval interval;
+    double root = 0;
+    try
+    {
+        root = fixedPoint(f, g, -0.5, 20, TOLERANCE);
+    }
+    catch(IterException &e)
+    {
+        printf("%s\n", e.what());
+    }
+
+    printf("%e\n", root);
     
-    interval = incrementalSearch(f, -1, 0.0001, 25000);
 
-    if(interval.wasSuccessful){
-        if (interval.isRoot) {
-            printf("The root is %f\n", interval.first);    
-        }
-        else {
-            printf("The root is inside %f and %f\n", interval.first, interval.last);
-            
-            int status;
-            double root;
-            
-            try {
-                root = bisection(f, interval.first, interval.last, 25, TOLERANCE, &status);
-            }
-            catch (IterException &iterEx) {
-                printf("%s\n", iterEx.what());
-                return EXIT_FAILURE;
-            }
-            catch (IntervalException &intervEx) {
-                printf("%s\n", intervEx.what());
-                return EXIT_FAILURE;
-            }
+    // Interval interval;
+    
+    // interval = incrementalSearch(f, -1, 0.0001, 25000);
 
-            if (status == EXACT_VALUE) {
-                printf("The exact root is: %e\n", root);
-            }
-            else if (status == APPROX_VALUE) {
-                printf("The root is: %e, with a tolerance of %.1e\n", root, TOLERANCE);
-            }
-        }
-    }
-    else{
-        printf("Nothing could be found\n");
-    }
+    // if(interval.wasSuccessful){
+    //     if (interval.isRoot) {
+    //         printf("The root is %f\n", interval.first);    
+    //     }
+    //     else {
+    //         printf("The root is inside %f and %f\n", interval.first, interval.last);
+            
+    //         int status;
+    //         double root;
+            
+    //         try {
+    //             root = bisection(f, interval.first, interval.last, 25, TOLERANCE, &status);
+    //         }
+    //         catch (IterException &iterEx) {
+    //             printf("%s\n", iterEx.what());
+    //             return EXIT_FAILURE;
+    //         }
+    //         catch (IntervalException &intervEx) {
+    //             printf("%s\n", intervEx.what());
+    //             return EXIT_FAILURE;
+    //         }
+
+    //         if (status == EXACT_VALUE) {
+    //             printf("The exact root is: %e\n", root);
+    //         }
+    //         else if (status == APPROX_VALUE) {
+    //             printf("The root is: %e, with a tolerance of %.1e\n", root, TOLERANCE);
+    //         }
+    //     }
+    // }
+    // else{
+    //     printf("Nothing could be found\n");
+    // }
 }
 
 double f(double x) {
-    return exp(2*x)+5*x;
+    // return exp(2*x)+5*x;
     //  return x-3;
+    return x*exp(x)-pow(x,2)-5*x-3;
+}
+
+double g(double x){
+    return 3/(exp(x)-x-5);
 }
--- src/singleVariableEquations/openMethods/fixedPoint.cpp
+++ src/singleVariableEquations/openMethods/fixedPoint.cpp
@@ -1,1 +1,26 @@
 #include "fixedPoint.h"
+#include <cmath>
+#include "../../../lib/exceptions.h"
+
+double fixedPoint(double (*func)(double), double (*gFunc)(double), double xa, int nIter, double tol) {
+    double fx = func(xa);
+    int count = 0;
+    double error = tol + 1;
+    double xn = 0;
+    while(fx != 0 && error > tol && count < nIter){
+        xn = gFunc(xa);
+        fx = func(xn);
+        error = fabs(xn - xa);
+        xa = xn;
+        count++;
+    }
+    if(fx == 0){
+        return xa;
+    }
+    else if(error < tol){
+        return xa;
+    }
+    else{
+        throw IterException();
+    }
+}
