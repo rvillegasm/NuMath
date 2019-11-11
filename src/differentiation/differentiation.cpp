fd1b60ccc39b052f0da0a66c6d785d80c99ef0b4
lmvasquezg <lmvasquezg@eafit.edu.co>
2019-11-10 19:24:22 -0500

    Comment on differentiation

--- src/differentiation/differentiation.cpp
+++ src/differentiation/differentiation.cpp
@@ -13,23 +13,32 @@ namespace numath{
     namespace differentiation{
         double differentiation( std::vector<Point> &points, int direction, double h, int num ){
             if(num==2){
+                //Two points
                 return (points[1].y-points[0].y)/h;
             }else if(num==3){
+                //Uses the first three points
                 if(direction==0){
+                    // Three points backwards
                     return (points[0].y-4*points[1].y+3*points[2].y)/(2*h);
                 }else if (direction==1){
+                    // Three points forward
                     return (-points[2].y+4*points[1].y-3*points[0].y)/(2*h);
                 }else if(direction ==2){  
+                    // Three points center
                     return (-points[0].y+points[2].y)/(2*h);
                 }else{
                     throw MethodException();
                 }
             }else if(num==5){
+                //Uses the first five points
                 if(direction==0){
+                    // Five points backwards
                     return ((25*points[4].y)-(48*points[3].y)+(36*points[2].y)-(16*points[1].y)+(3*points[0].y))/(12*h);
-                }else if (direction==1){               
+                }else if (direction==1){    
+                    // Five points forward          
                     return  (-25*points[0].y+48*points[1].y-36*points[2].y+16*points[3].y-3*points[4].y)/(12*h);
                 }else if(direction ==2){
+                    // Five points center
                     return (points[0].y-8*points[1].y+8*points[3].y-points[4].y)/(12*h);
                 }else{
                     throw MethodException();
