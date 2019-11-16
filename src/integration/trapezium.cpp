9456523b54b9092be2488ac996e847e5abe56a53
rvillegasm <rvillegasm@eafit.edu.co>
2019-11-16 11:34:26 -0500

    Changing minor stuff in trapezium

--- src/integration/trapezium.cpp
+++ src/integration/trapezium.cpp
@@ -1,21 +1,22 @@
 #include "trapezium.h"
+#include <omp.h>
 
 namespace numath {
     namespace integration {
 
-       double trapezium(std::vector<Point> &points) {
+        double trapezium(std::vector<Point> &points) {
 
-           const unsigned int N_POINTS = points.size();
+            const unsigned int N_POINTS = points.size();
 
-           double h = (points[N_POINTS-1].x - points[0].x) / (N_POINTS - 1);
+            double h = (points[N_POINTS-1].x - points[0].x) / (N_POINTS - 1);
 
-           double sum = 0;
-           for (unsigned int i = 1; i < N_POINTS - 1; i++) {
-               sum += points[i].y; 
-           }
+            double sum = 0;
+            for (unsigned int i = 1; i < N_POINTS - 1; i++) {
+                sum += points[i].y; 
+            }
 
-           return (h / 2) * (points[0].y + (2 * sum) + points[N_POINTS-1].y);
-       }
+            return (h / 2) * (points[0].y + (2 * sum) + points[N_POINTS-1].y);
+        }
        
     }
 }
